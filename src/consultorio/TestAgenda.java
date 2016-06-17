266835288a3fdf8923c153d007460f4ad0cc1f38
uade <uade@502PC04.labs>
2016-06-16 21:02:25 -0300

    Test profesor

---
+++ src/consultorio/TestAgenda.java
@@ -0,0 +1,111 @@
+package consultorio;
+
+import implementacion.AgendaDinamica;
+import tda.IAgenda;
+import tda.TDACola;
+import tda.TDAConjunto;
+
+public class TestAgenda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IAgenda agenda = new AgendaDinamica();
+		agenda.inicializar();
+		agenda.agregar("Juan"	,	"20160620"	,	"Carlos"	,"05:45");
+		agenda.agregar("Juan"	,	"20160620"	,	"Jose"		,"12:45");
+		agenda.agregar("Juan"	,	"20160620"	,	"Juan"		,"15:45");
+		agenda.agregar("Juan"	,	"20160620"	,	"Jeronimo"	,"11:45");
+		agenda.agregar("Juan"	,	"20160620"	,	"Jacinto"	,"10:45");
+		agenda.agregar("Juan"	,	"20160620"	,	"Joaquin"	,"13:45");
+		agenda.agregar("Juan"	,	"20160620"	,	"Javier"	,"08:30");
+		agenda.agregar("Juan"	,	"20160620"	,	"Julieta"	,"16:15");
+		agenda.agregar("Juan"	,	"20160620"	,	"Juliana"	,"09:00");
+		agenda.agregar("Juan"	,	"20160623"	,	"Antonia"	,"05:45");
+		agenda.agregar("Juan"	,	"20160623"	,	"Carlos"	,"10:45");
+		agenda.agregar("Juan"	,	"20160623"	,	"Ana"		,"13:45");
+		agenda.agregar("Juan"	,	"20160623"	,	"Alma"		,"08:30");
+		agenda.agregar("Juan"	,	"20160623"	,	"Aldana"	,"16:15");
+		agenda.agregar("Juan"	,	"20160624"	,	"Carlos"	,"09:00");
+		agenda.agregar("Juan"	,	"20160624"	,	"Maria"		,"10:00");
+		agenda.agregar("Juan"	,	"20160624"	,	"Carlos"	,"11:00");
+		agenda.agregar("Juan"	,	"20160624"	,	"Maria"		,"12:00");
+		agenda.agregar("Juan"	,	"20160624"	,	"Carlos"	,"13:00");
+		agenda.agregar("Juan"	,	"20160624"	,	"Maria"		,"14:00");
+		agenda.agregar("Juan"	,	"20160624"	,	"Carlos"	,"15:00");
+/** Juan */
+		agenda.agregar(	"Marcela"	,	"20160620"	,	"Jose"		,	"12:45"	);
+		agenda.agregar(	"Marcela"	,	"20160620"	,	"Marcela"	,	"15:45"	);
+		agenda.agregar(	"Marcela"	,	"20160620"	,	"Jeronimo"	,	"11:45"	);
+		agenda.agregar(	"Marcela"	,	"20160620"	,	"Jacinto"	,	"10:45"	);
+		agenda.agregar(	"Marcela"	,	"20160622"	,	"Joaquin"	,	"13:45"	);
+		agenda.agregar(	"Marcela"	,	"20160622"	,	"Javier"	,	"08:30"	);
+		agenda.agregar(	"Marcela"	,	"20160620"	,	"Julieta"	,	"16:15"	);
+		agenda.agregar(	"Marcela"	,	"20160620"	,	"Juliana"	,	"09:00"	);
+		agenda.agregar(	"Marcela"	,	"20160623"	,	"Antonia"	,	"05:45"	);
+		agenda.agregar(	"Marcela"	,	"20160623"	,	"Carlos"	,	"10:45"	);
+		agenda.agregar(	"Marcela"	,	"20160623"	,	"Ana"		,	"13:45"	);
+		agenda.agregar(	"Marcela"	,	"20160623"	,	"Alma"		,	"08:30"	);
+		agenda.agregar(	"Marcela"	,	"20160623"	,	"Aldana"	,	"16:15"	);
+		agenda.agregar(	"Marcela"	,	"20160624"	,	"Carlos"	,	"09:00"	);
+		agenda.agregar(	"Marcela"	,	"20160624"	,	"Maria"		,	"10:00"	);
+		agenda.agregar(	"Marcela"	,	"20160624"	,	"Carlos"	,	"11:00"	);
+		agenda.agregar(	"Marcela"	,	"20160625"	,	"Maria"		,	"12:00"	);
+		agenda.agregar(	"Marcela"	,	"20160625"	,	"Carlos"	,	"13:00"	);
+		agenda.agregar(	"Marcela"	,	"20160625"	,	"Maria"		,	"11:00"	);
+		agenda.agregar(	"Marcela"	,	"20160625"	,	"Carlos"	,	"10:00"	);
+/** Marcela */
+		agenda.agregar(	"Evaristo"	,	"20160630"	,	"Eustaquio"	,	"12:00"	);
+/** Evaristo */
+		
+		recorrerAgenda(agenda);
+		
+		agenda.eliminarTurno("Evaristo", "20160630", "Eustaquio");
+		recorrerAgenda(agenda);
+		
+		agenda.eliminarTurno("Marcela", "20160625", "Carlos");
+		recorrerAgenda(agenda);
+		
+		agenda.eliminar("Juan");
+		recorrerAgenda(agenda);
+	}
+
+	private static void recorrerAgenda(IAgenda agenda) {
+		// TODO Auto-generated method stub
+		String medico;
+		/* Recorro la estructura por médicos */
+		TDAConjunto medicos = agenda.obtenerMedicos();
+		while(!medicos.conjuntoVacio())
+		{	
+			medico = medicos.elegir();
+			System.out.println("medico : " + medico);
+			System.out.println(" fecha \t paciente \t turno");
+			String[][] turnos = agenda.obtenerTurnosMedico(medico);
+			muestroDatos(turnos);
+			medicos.sacar(medico);
+		}
+				
+		/* Recorro la estructura por fechas */	
+		TDACola fechas = agenda.obtenerFechas();
+		System.out.println(" fecha \t medico\t paciente \t turno");
+		while(!fechas.colaVacia())
+		{
+			String[][] turnos = agenda.obtenerTurnosFecha(fechas.primero());
+			muestroDatos(turnos);
+			fechas.desacolar();
+		}
+	}
+
+	private static void muestroDatos(String[][] turnos) {
+		// TODO Auto-generated method stub
+		int cols = turnos.length;
+		int rows = turnos[0].length;
+		for(int j = 0; j<rows; j++){
+			for(int i = 0; i<cols; i++)
+			{
+				System.out.print(turnos[i][j] + "\t");
+			}
+			System.out.println("");
+		}
+	}
+
+}
